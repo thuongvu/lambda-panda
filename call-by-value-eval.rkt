f160d192e86aec40944caf40d9c4151ee849550b
thuongvu <thuongvuho@gmail.com>
2015-09-28 23:59:40 -0700

    refactor: add a procedure that will take care of recursion of evaluation to better abstract call by value reduce proc

--- call-by-value-eval.rkt
+++ call-by-value-eval.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require "substitute.rkt")
 
-(define (eval-term term)
+(define (cbv-reduce term)
   (match term
       ; Primitive values
       [(? string?) term]
@@ -11,34 +11,32 @@
       ; Identity
       [`(λ (,v) ,v) term]
 
-      ; Application
-       [`((λ (,v1) ,b1) ,e)  (reduce-application `(,(car term) ,(eval-term e)))]
-
-      ; Application-abstraction
-      [`(,f ,e) `(,(eval-term f) ,e)]
-    ))
-
-(define (reduce-application term)
-  (match term
-      ; Application: there are two scenarios to account for: the lhs or rhs are b-redexes.
-  
+      ; Application: there are two scenarios to account for: the lhs or rhs are b-redexes.  
       ; (reducible-term1            reducible-term2)      = (reducible-term1 reduced-term2)
-      [`((λ (,v1) ,b1),   (and rhs `(λ (,v2) ,b2)))         (eval-term (substitute b1 v1 rhs))]
+      [`((λ (,v1) ,b1),   (and rhs `(λ (,v2) ,b2)))         (substitute b1 v1 rhs)]
    
       ; (reducible-term1  reduced-term2)                  = (reduced-term1 reduced-term2) ==> evaluated-term
       [`((λ (,v) ,b)      ,e)                               (substitute b v e)]
+
+      ; Application-abstraction
+      [`(,f ,e) `(,(cbv-reduce f) ,e)]
     ))
+
+(define (recursive-eval term)
+  (if (equal? term (cbv-reduce term))
+              term
+              (recursive-eval (cbv-reduce term))))
 
 
 ;TESTS for eval
-(eval-term `((λ(y) (y a)) (λ(x)x)    )) ; 'a
-(eval-term `(λ(x)x) ) ;'(λ (x) x)
-(eval-term `((λ(x)x)y)) ; 'y
-(eval-term `((λ(x)x) (λ(x)x)) ) ;'(λ (x) x)
-(eval-term `((λ(y) (y a)) (λ(x)x)    )) ;'a
-(eval-term '((λ (x) x) a)) ;'a
-(eval-term `( (λ (x) (x x)) (λ (y) y) )  ) ;'(λ (y) y)
-(eval-term `((λ(f) (f 7)) (λ (y) y))) ;7
-(eval-term `((λ(f) (f 7)) ((λ (x) (x x)) (λ (y) y)) )) ;7
-(eval-term `((λ(f) (f 7)) (λ(z) z))) ;7
-;(eval-term `((λ(x)(x x)) (λ(x)(x x))))
+(recursive-eval `((λ(y) (y a)) (λ(x)x)    ))               ; 'a
+(recursive-eval `(λ(x)x) )                                 ;'(λ (x) x)
+(recursive-eval `((λ(x)x)y))                               ; 'y
+(recursive-eval `((λ(x)x) (λ(x)x)) )                       ;'(λ (x) x)
+(recursive-eval `((λ(y) (y a)) (λ(x)x)    ))               ;'a
+(recursive-eval '((λ (x) x) a))                            ;'a
+(recursive-eval `( (λ (x) (x x)) (λ (y) y) )  )            ;'(λ (y) y)
+(recursive-eval `((λ(f) (f 7)) (λ (y) y)))                 ;7
+(recursive-eval `((λ(f) (f 7)) ((λ (x) (x x)) (λ (y) y)))) ;7
+(recursive-eval `((λ(f) (f 7)) (λ(z) z)))                  ;7
+;(recursive-eval `((λ(x)(x x)) (λ(x)(x x))))
